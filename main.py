bfd73c4e8772bce71f268bd3daa25c346ae4ae7f
littleaxa <aksha12081@gmail.com>
2024-09-23 17:04:50 +0530

    Board and player input created

---
+++ main.py
@@ -0,0 +1,18 @@
+#creating board 
+board = [" ", " ", " ",
+         " ", " ", " ",
+         " ", " ", " "]
+winner = None
+
+#taking player input and printing board 
+print("Welcome to Tic Tak Toe")
+one = input("Enter user 1:")
+two = input("Enter user 2:")
+print(board [0] + " | " + board[1] + " | " + board[2])
+print("----------")
+print(board [3] + " | " + board[4] + " | " + board[5])
+print("----------")
+print(board [6] + " | " + board[7] + " | " + board[8])
+
+
+
